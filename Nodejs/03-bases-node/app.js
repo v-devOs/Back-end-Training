26f7e44ec798afd4594d30f6807aa3c0eb420630
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2023-09-27 16:26:42 -0600

    Comenzando con la primer aplicación de consola

--- Nodejs/03-bases-node/app.js
+++ Nodejs/03-bases-node/app.js
@@ -2,8 +2,8 @@ const { crearArchivo } = require('./helpers/multiplicar')
 
 console.clear()
 
-const base = 4
-
+const [ , , arg3 = '--base=5'] = process.argv  
+const [, base = 5 ] = arg3.split('=')
 
 crearArchivo( base )
   .then( msg => console.log(msg) )
