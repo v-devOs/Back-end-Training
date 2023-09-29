675676bebef792c8cdd54fef407cf654feebc605
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2023-09-28 21:21:13 -0600

    Primer hola mundo

--- C#/HolaMundo/HolaMundo/Program.cs
+++ C#/HolaMundo/HolaMundo/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HolaMundo
 {
@@ -11,6 +7,8 @@ namespace HolaMundo
         static void Main(string[] args)
         {
             Console.WriteLine("Hola mundo");
+            Console.WriteLine("Hola soy uriel");
+            Console.Read();
         }
     }
 }
