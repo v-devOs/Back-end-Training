a0ce2960beaf792394d6ba314c8eb53b374f9b8c
Uriel Emiliano Galindo Lopez <ugalu448@gmail.com>
2023-10-28 11:56:16 -0600

    Fin del curso de fazt

--- Docker/02-fazt/server.js
+++ Docker/02-fazt/server.js
@@ -4,19 +4,24 @@ import postgres from 'postgres'
 const app = express()
 
 const sql = postgres({
-  host: 'localhost',
+  host: 'pg_database',
   port: 5432,
   user: 'postgres',
-  password: '123'
+  password: 'password'
+})
+
+app.get('/', async(_req, res) => {
+
+  const resp = await sql`select 1 + 1`
+
+  res.json({
+    message: 'Hola mundo',
+    consulta: resp
+  })
 })
 
 
 
-
-
-
-app.listen(3000, async() => {
+app.listen(5000, async() => {
   console.log('Servidor en linea')
-  const resp = await sql`select 1 + 1`
-  console.log(resp)
 })
